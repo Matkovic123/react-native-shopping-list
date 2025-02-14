2cfe002558061a67e91a937382c1422020bfe098
tmatkovic <toni.matkovic0@gmail.com>
2025-02-14 13:36:30 +0000

    Modals

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,18 @@ export default function Layout() {
         name="index"
         options={{ title: "Shopping List" }}
       ></Stack.Screen>
+      <Stack.Screen
+        name="counter"
+        options={{ title: "Counter", presentation: "modal", animation: "fade" }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      ></Stack.Screen>
     </Stack>
   );
 }
