ee8361faf383837152ebf142345005bc717f7956
tmatkovic <toni.matkovic0@gmail.com>
2025-02-17 14:50:30 +0000

    finished timer w intervals & dates

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,19 +1,40 @@
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { theme } from "../../theme";
-import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import { intervalToDuration, isBefore } from "date-fns";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import { SchedulableTriggerInputTypes } from "expo-notifications";
 import { useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import { TimeSegment } from "../../components/TimeSegment";
+
+//10s from now
+const timeStamp = Date.now() + 10 * 1000;
+
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: ReturnType<typeof intervalToDuration>;
+};
 
 export default function CounterScreen() {
-  const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const [countdownStatus, setCountdownStatus] = useState<CountdownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setSecondsElapsed((val) => val + 1);
+      const isOverdue = isBefore(timeStamp, Date.now());
+      const distance = intervalToDuration(
+        isOverdue
+          ? { start: timeStamp, end: Date.now() }
+          : { start: Date.now(), end: timeStamp },
+      );
+      setCountdownStatus({ isOverdue, distance });
     }, 1000);
     return () => clearInterval(intervalId); // basically onUnmount
   }, []);
+
+  console.log(countdownStatus);
 
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
@@ -37,14 +58,54 @@ export default function CounterScreen() {
     }
   };
   return (
-    <View style={styles.container}>
-      <Text>{secondsElapsed}</Text>
+    <View
+      style={[
+        styles.container,
+        countdownStatus.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {countdownStatus.isOverdue ? (
+        <Text
+          style={[
+            styles.heading,
+            countdownStatus.isOverdue ? styles.whiteText : undefined,
+          ]}
+        >
+          Time is up
+        </Text>
+      ) : (
+        <>
+          <Text>Due in...</Text>
+        </>
+      )}
+      <View style={styles.row}>
+        <TimeSegment
+          unit="Days"
+          number={countdownStatus.distance.days || 0}
+          textStyle={countdownStatus.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={countdownStatus.distance.hours || 0}
+          textStyle={countdownStatus.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={countdownStatus.distance.minutes || 0}
+          textStyle={countdownStatus.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={countdownStatus.distance.seconds || 0}
+          textStyle={countdownStatus.isOverdue ? styles.whiteText : undefined}
+        />
+      </View>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
         onPress={scheduleNotification}
       >
-        <Text style={styles.buttonText}>Schedule notificatiion</Text>
+        <Text style={styles.buttonText}>Done</Text>
       </TouchableOpacity>
     </View>
   );
@@ -67,5 +128,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textTransform: "uppercase",
     letterSpacing: 1,
+  },
+  row: { flexDirection: "row", marginBottom: 22 },
+  heading: { fontSize: 24, fontWeight: "bold", marginBottom: 24 },
+  containerLate: {
+    backgroundColor: theme.colorRed,
+  },
+  whiteText: {
+    color: theme.colorWhite,
   },
 });
